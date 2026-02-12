3b156f3e09a9543ab82b5993679dd5965a8998f3
catmāoneko猫 <hellojunchul@gmail.com>
2026-02-12 09:56:12 +0300

    Started making king. Updated board to 2d array

--- src/main/Chess/board.java
+++ src/main/Chess/board.java
@@ -1,13 +1,10 @@
 public class board {
-	final boolean[] x;
-	final boolean[] y;
-	boolean occupied;
+	final piece[][] location;
 
 	//supposed to be 2d array that only tells if the board is occupied or not. 
 	//May need many changes such as making it a array of pieces
 	//This board is orientated so that the white pieces are at the bottom and the black are at the top
-	public board(int xSize, int ySize){
-		x=new boolean[xSize];
-		y=new boolean[ySize];
+	public board(int size){
+		location=new piece[size][size];
 	}
 }
