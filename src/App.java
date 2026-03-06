127433cf06ed3febc388f30b6fa62dd9630d3937
EmperorGrock <Grahambohill@hotmail.com>
2026-03-06 10:30:16 +0300

    Added games to App

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import main.cardgames.CardGamesRunner;
 import main.tictactoe.TicTacToe;
 
 public class App {
-    private static String[] games = {"Quit", "Card Games", "TicTacToe"};
+    private static String[] games = {"Quit", "Card Games", "TicTacToe", "Chess", "Monopoly"};
     public static void main(String[] args){
         
         
