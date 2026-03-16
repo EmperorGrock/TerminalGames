6e11901a62f8a43621f5db025f33a803d6823610
EmperorGrock <Grahambohill@hotmail.com>
2026-03-16 15:43:25 +0300

    Refactor 3D map to use TwoDHelper
    Replace the raw String[][][] map with an array of TwoDHelper instances for each z-layer. Add a no-arg constructor (default 4x4x4), initialize per-layer TwoDHelper objects, and delegate clearing and access to those helpers. Implement printMap and printWideMap for formatted output, add getXMax/getYMax/getZMax and adapted getLocation, and include a small main() demo. This also corrects the previous clearMap looping logic and updates the compiled class file.

--- src/main/extramethods/ThreeDHelper.java
+++ src/main/extramethods/ThreeDHelper.java
@@ -1,42 +1,99 @@
 package main.extramethods;
 
 public class ThreeDHelper {
-	private int xMax;
-	private int yMax;
 	private int zMax;
-	private String[][][] map;
+	private TwoDHelper[] map;
 
-	public ThreeDHelper(int xMax, int yMax, int zMax){
-		this.xMax = xMax;
-		this.yMax = yMax;
-		this.zMax = zMax;
-		map = new String[zMax][yMax][xMax];
+	public ThreeDHelper(){
+		this.zMax = 4;
+		map = new TwoDHelper[4];
+		for(int z = 0; z < 4; z++){
+			map[z] = new TwoDHelper(4, 4);
+		}
 		clearMap();
 	}
 
+	public ThreeDHelper(int xMax, int yMax, int zMax){
+		this.zMax = zMax;
+		map = new TwoDHelper[zMax];
+		for(int z = 0; z < zMax; z++){
+			map[z] = new TwoDHelper(xMax, yMax);
+		}
+		clearMap();
+	}
+
+	public void printMap(){
+		for(int z = 0; z < zMax; z++){
+			System.out.print(" ");
+			for(int i = 1; i <= map[z].getXMax(); i++){
+				System.out.print(" " + i);
+			}
+			System.out.print("   ");
+		}
+		System.out.println();
+		for(int y = 0; y < map[0].getYMax(); y++){
+			for(int z = 0; z < zMax; z++){
+				System.out.print((y+1) + " ");
+				System.out.print(map[z].formatLine(y));
+				System.out.print("   ");
+			}
+			System.out.println();
+			if(y < map[0].getYMax()-1){
+				for(int z = 0; z < zMax; z++){
+					System.out.print(map[z].getBorder()+"   ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	public void printWideMap(){
+		for(int z = 0; z < zMax; z++){
+			for(int i = 1; i <= map[z].getXMax(); i++){
+				System.out.print("   " + i);
+			}
+			System.out.print("    ");
+		}
+		System.out.println();
+		for(int y = 0; y < map[0].getYMax(); y++){
+			for(int z = 0; z < zMax; z++){
+				System.out.print((y+1) + " ");
+				System.out.print(map[z].formatWideLine(y));
+				System.out.print("   ");
+			}
+			System.out.println();
+			if(y < map[0].getYMax()-1){
+				for(int z = 0; z < zMax; z++){
+					System.out.print(map[z].getWideBorder()+"   ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
 	public void clearMap(){
-		for(int x = 0; x < xMax; x++)
-			for(int y = 0; x < yMax; y++)
-				for(int z = 0; z < zMax; z++)
-					map[z][y][x] = " ";
+		for(int z = 0; z < zMax; z++)
+			map[z].clearMap();
 	}
 
 	public String getLocation(int x, int y, int z){
-		x -= 1;
-		y -= 1;
-		z -= 1;
-		return map[z][y][x];
-	}
-
-	public int getXMax(){
-		return xMax;
-	}
-
-	public int getYMax(){
-		return yMax;
+		return map[z-1].getLocation(x, y);
 	}
 	
 	public int getZMax(){
 		return zMax;
+	}
+
+	public int getYMax(){
+		return map[0].getYMax();
+	}
+
+	public int getXMax(){
+		return map[0].getXMax();
+	}
+
+	void main(){
+		ThreeDHelper test = new ThreeDHelper();
+		test.printWideMap();
 	}
 }
